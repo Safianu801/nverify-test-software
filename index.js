f207c2ab5be366d1621b522c82b172a9b284a6e2
sufyan <safianumoney100@gmail.com>
2026-05-25 20:27:32 +0100

    Code update

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ localIo.use((socket, next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-const SAAS_SOCKET_URL = process.env.SAAS_SOCKET_URL;
-// const SAAS_SOCKET_URL = process.env.SAAS_SOCKET_URL || "http://localhost:5000";
+const SAAS_SOCKET_URL = process.env.SAAS_SOCKET_URL || "http://localhost:5000";
 
 localIo.on("connection", (socket) => {
   const endUserId = socket.identity?.endUserID || socket.identity?.userID;
@@ -34,10 +33,63 @@ localIo.on("connection", (socket) => {
     reconnection: true,
   });
 
+  // ==========================================
+  // 1. CHAT EVENTS (Untouched)
+  // ==========================================
+  saasSocket.on("chat:history", (data) => socket.emit("chat:history", data));
+  saasSocket.on("chat:newMessage", (msg) =>
+    socket.emit("chat:newMessage", msg),
+  );
+  saasSocket.on("chat:error", (err) => socket.emit("chat:error", err));
+  saasSocket.on("chat:messageSent", (msg) =>
+    socket.emit("chat:messageSent", msg),
+  );
+  saasSocket.on("chat:messageDeleted", (data) =>
+    socket.emit("chat:messageDeleted", data),
+  );
+  saasSocket.on("chat:reactionUpdated", (data) =>
+    socket.emit("chat:reactionUpdated", data),
+  );
+  saasSocket.on("chat:messageRead", (data) =>
+    socket.emit("chat:messageRead", data),
+  );
+  saasSocket.on("chat:conversations", (data) =>
+    socket.emit("chat:conversations", data),
+  );
+  saasSocket.on("chat:typing", (data) => socket.emit("chat:typing", data));
+
+  socket.on("chat:joinChat", (data) => saasSocket.emit("chat:joinChat", data));
+  socket.on("chat:sendMessage", (data) =>
+    saasSocket.emit("chat:sendMessage", data),
+  );
+  socket.on("chat:getConversations", () =>
+    saasSocket.emit("chat:getConversations", { endUserId }),
+  );
+  socket.on("chat:addReaction", (data) =>
+    saasSocket.emit("chat:addReaction", data),
+  );
+  socket.on("chat:markAsRead", (data) =>
+    saasSocket.emit("chat:markAsRead", data),
+  );
+  socket.on("chat:deleteMessage", (data) =>
+    saasSocket.emit("chat:deleteMessage", data),
+  );
+  socket.on("chat:typing:start", (data) =>
+    saasSocket.emit("chat:typing:start", data),
+  );
+  socket.on("chat:typing:stop", (data) =>
+    saasSocket.emit("chat:typing:stop", data),
+  );
+
+  // ==========================================
+  // 2. VIDEO CALL EVENTS (New)
+  // ==========================================
+
   // Forward SaaS -> Local Client
-  saasSocket.on("video:iceConfig", (data) =>
-    socket.emit("video:iceConfig", data),
-  ); // CRITICAL LINE
+  saasSocket.on("video:error", (err) => socket.emit("video:error", err));
+  saasSocket.on("video:callHistory", (data) =>
+    socket.emit("video:callHistory", data),
+  );
   saasSocket.on("video:incomingCall", (data) =>
     socket.emit("video:incomingCall", data),
   );
@@ -47,17 +99,25 @@ localIo.on("connection", (socket) => {
   saasSocket.on("video:userJoined", (data) =>
     socket.emit("video:userJoined", data),
   );
+  saasSocket.on("video:participants", (data) =>
+    socket.emit("video:participants", data),
+  );
   saasSocket.on("video:offer", (data) => socket.emit("video:offer", data));
   saasSocket.on("video:answer", (data) => socket.emit("video:answer", data));
   saasSocket.on("video:iceCandidate", (data) =>
     socket.emit("video:iceCandidate", data),
   );
+  saasSocket.on("video:mediaStateChanged", (data) =>
+    socket.emit("video:mediaStateChanged", data),
+  );
   saasSocket.on("video:userLeft", (data) =>
     socket.emit("video:userLeft", data),
   );
-  saasSocket.on("video:error", (err) => socket.emit("video:error", err));
 
   // Forward Local Client -> SaaS
+  socket.on("video:getCallHistory", (data) =>
+    saasSocket.emit("video:getCallHistory", data),
+  );
   socket.on("video:initiateCall", (data) =>
     saasSocket.emit("video:initiateCall", data),
   );
@@ -72,16 +132,11 @@ localIo.on("connection", (socket) => {
   socket.on("video:iceCandidate", (data) =>
     saasSocket.emit("video:iceCandidate", data),
   );
+  socket.on("video:toggleMedia", (data) =>
+    saasSocket.emit("video:toggleMedia", data),
+  );
   socket.on("video:leaveCall", (data) =>
     saasSocket.emit("video:leaveCall", data),
-  );
-
-  // Chat events... (omitted for brevity, keep your existing ones)
-  saasSocket.on("chat:newMessage", (msg) =>
-    socket.emit("chat:newMessage", msg),
-  );
-  socket.on("chat:sendMessage", (data) =>
-    saasSocket.emit("chat:sendMessage", data),
   );
 
   socket.on("disconnect", () => saasSocket.disconnect());

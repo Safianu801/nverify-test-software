db6ab8facf8c0b5067723c566ac898dc5800797f
sufyan <safianumoney100@gmail.com>
2026-05-27 15:36:10 +0100

    code update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io"); // Note: Ensure this is "socket.io" in your package.json
+const { Server } = require("socket.io");
 const ioClient = require("socket.io-client");
 require("dotenv").config();
 
@@ -34,29 +34,22 @@ localIo.on("connection", (socket) => {
   });
 
   // ==========================================
-  // 1. CHAT EVENTS (Untouched)
+  // 1. CHAT EVENTS (Piped through)
   // ==========================================
-  saasSocket.on("chat:history", (data) => socket.emit("chat:history", data));
-  saasSocket.on("chat:newMessage", (msg) =>
-    socket.emit("chat:newMessage", msg),
+  const chatEvents = [
+    "chat:history",
+    "chat:newMessage",
+    "chat:error",
+    "chat:messageSent",
+    "chat:messageDeleted",
+    "chat:reactionUpdated",
+    "chat:messageRead",
+    "chat:conversations",
+    "chat:typing",
+  ];
+  chatEvents.forEach((event) =>
+    saasSocket.on(event, (data) => socket.emit(event, data)),
   );
-  saasSocket.on("chat:error", (err) => socket.emit("chat:error", err));
-  saasSocket.on("chat:messageSent", (msg) =>
-    socket.emit("chat:messageSent", msg),
-  );
-  saasSocket.on("chat:messageDeleted", (data) =>
-    socket.emit("chat:messageDeleted", data),
-  );
-  saasSocket.on("chat:reactionUpdated", (data) =>
-    socket.emit("chat:reactionUpdated", data),
-  );
-  saasSocket.on("chat:messageRead", (data) =>
-    socket.emit("chat:messageRead", data),
-  );
-  saasSocket.on("chat:conversations", (data) =>
-    socket.emit("chat:conversations", data),
-  );
-  saasSocket.on("chat:typing", (data) => socket.emit("chat:typing", data));
 
   socket.on("chat:joinChat", (data) => saasSocket.emit("chat:joinChat", data));
   socket.on("chat:sendMessage", (data) =>
@@ -82,60 +75,45 @@ localIo.on("connection", (socket) => {
   );
 
   // ==========================================
-  // 2. VIDEO CALL EVENTS (Updated with TURN/ICE Support)
+  // 2. VIDEO CALL EVENTS (Conference Enabled)
   // ==========================================
 
-  // --- Forward SaaS -> Local Client ---
+  // SaaS -> Proxy -> Frontend
   saasSocket.on("video:error", (err) => socket.emit("video:error", err));
-  saasSocket.on("video:callHistory", (data) =>
-    socket.emit("video:callHistory", data),
-  );
   saasSocket.on("video:incomingCall", (data) =>
     socket.emit("video:incomingCall", data),
   );
   saasSocket.on("video:callResponse", (data) =>
     socket.emit("video:callResponse", data),
   );
+  saasSocket.on("video:ready", (data) => socket.emit("video:ready", data));
   saasSocket.on("video:userJoined", (data) =>
     socket.emit("video:userJoined", data),
   );
-  saasSocket.on("video:participants", (data) =>
-    socket.emit("video:participants", data),
+  saasSocket.on("video:userLeft", (data) =>
+    socket.emit("video:userLeft", data),
   );
-
-  // NEW: Receives Twilio ICE servers from SaaS and sends to Frontend
+  saasSocket.on("video:mediaStateChanged", (data) =>
+    socket.emit("video:mediaStateChanged", data),
+  );
   saasSocket.on("video:iceConfig", (data) =>
     socket.emit("video:iceConfig", data),
   );
 
+  // Signaling (Must include targetId)
   saasSocket.on("video:offer", (data) => socket.emit("video:offer", data));
   saasSocket.on("video:answer", (data) => socket.emit("video:answer", data));
   saasSocket.on("video:iceCandidate", (data) =>
     socket.emit("video:iceCandidate", data),
   );
-  saasSocket.on("video:mediaStateChanged", (data) =>
-    socket.emit("video:mediaStateChanged", data),
-  );
-  saasSocket.on("video:userLeft", (data) =>
-    socket.emit("video:userLeft", data),
-  );
 
-  // --- Forward Local Client -> SaaS ---
-  socket.on("video:getCallHistory", (data) =>
-    saasSocket.emit("video:getCallHistory", data),
-  );
+  // Frontend -> Proxy -> SaaS
   socket.on("video:initiateCall", (data) =>
     saasSocket.emit("video:initiateCall", data),
   );
   socket.on("video:respondToCall", (data) =>
     saasSocket.emit("video:respondToCall", data),
   );
-
-  // NEW: Manual request for ICE servers from Frontend to SaaS
-  socket.on("video:getIceServers", (data) =>
-    saasSocket.emit("video:getIceServers", data),
-  );
-
   socket.on("video:joinRoom", (data) =>
     saasSocket.emit("video:joinRoom", data),
   );
